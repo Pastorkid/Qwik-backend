63bae58b71a65724636e3b4d6bc4261bf2230b31
digvijay-md <digvijay.bind@mktdynamics.com>
2023-10-07 23:09:53 +0530

    auth

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,28 +1,37 @@
-// middleware/authMiddleware.js
+const Operator = require("../db/Operator");
 const jwt = require("jsonwebtoken");
-const secretKey = process.env.JWT_SECRET; // Replace with your actual secret key
-const token = require("../configs/jwtToken");
-function authMiddleware(req, res, next) {
-  // Get the token from the request headers
-  const token = req.header("Authorization");
 
-  // Check if a token is present
-  if (!token) {
-    return res.status(401).json({message: "Unauthorized"});
-  }
-
-  // Verify the token
-  jwt.verify(token, secretKey, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({message: "Token is not valid"});
+const authMiddleware = async (req, res, next) => {
+  let token;
+  if (req?.headers.authorization?.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+    try {
+      if (token) {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decoded);
+        const operator = await Operator.findById(decoded?.id);
+        req.operator = operator;
+        next();
+      }
+    } catch (error) {
+      console.log("No Authorized token, token expired , Please LOgin again");
     }
+  } else {
+    console.log("No Bearer token attached to header");
+  }
+};
 
-    // Attach the decoded payload (user information) to the request object
-    req.user = decoded;
-
-    // Continue to the next middleware or route
-    next();
-  });
-}
-
-module.exports = authMiddleware;
+// const isAdmin=asyncHandler(async(req,res,next)=>{
+//   const {email}=req.admin;
+//   const adminUser=await prisma.Admin.findUnique({
+//     where:{
+//       email:email
+//     }
+//   });
+//   if(adminUser.role!=="Admin"){
+//       throw new Error("Only admin can access this route")
+//   }else{
+//     next();
+//   }
+// })
+module.exports = {authMiddleware};
--- routes/Operator-Router.js
+++ routes/Operator-Router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const OperatorController = require("../controller/C-Operator");
-const authMiddleware = require("../middleware/authMiddleware");
+const {authMiddleware} = require("../middleware/authMiddleware");
 const asyncMiddleware = require("../middleware/async-middleware");
 // router.patch(
 //   "/editaircraft",
